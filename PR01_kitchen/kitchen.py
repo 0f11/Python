c35663eb683945aa66d881a172920dc1708d06dc
Rommi Parman <rommi.parman@ttu.ee>
2018-09-07 09:38:20 +0300

    v2test2

--- PR01_kitchen/kitchen.py
+++ PR01_kitchen/kitchen.py
@@ -10,7 +10,7 @@ print("Welcome to the kitchen.")
 name = input("What is your name? ")
 # Tervita kasutajat: Hi there, [nimi]!.
 print(f"Hi there, {name}!")
-# Küsi kasutajalt, mida ta juua soovib. Salvesta see muutujasse
+# Küsi kasutajalt, mida ta juua soovib. Salvesta see muutujasse.
 drink = input("What would you like to drink?")
 # Kui kasutaja vastas tea, kuvab programm ekraanile Have a nice tea!
 if drink == "tea":
