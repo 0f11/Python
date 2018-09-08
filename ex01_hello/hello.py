7f28c6432e9b7c3249d8129027b5307991aa213d
Rommi Parman <rommi.parman@ttu.ee>
2018-09-08 22:16:00 +0300

    test

---
+++ ex01_hello/hello.py
@@ -0,0 +1,12 @@
+"""
+"""
+name = input("Hello! Whats your name?")
+
+if name == "rommi":
+    print("Name was not inserted!")
+else:
+    print("Name was not inserted!")
+school = input("Where do you study?")
+
+if school =
+    print("School was not entered!")
