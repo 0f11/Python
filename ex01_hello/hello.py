535f0227c1da5a1ab764a50c9a7244ea7ce4564e
Rommi Parman <rommi.parman@ttu.ee>
2018-09-09 10:50:33 +0300

    v1

--- ex01_hello/hello.py
+++ ex01_hello/hello.py
@@ -1,5 +1,7 @@
 """
-Hello.py. is a script for claclulating body mass index.
+Hello.py.
+
+is a script for calculating body mass index.
 """
 name = input("Hello! What's your name?")
 
@@ -13,23 +15,14 @@ if school == "":
 else:
     print(f"{name}, welcome to {school}!")
 
-weight = float(input("Whats your weight?"))
-height = float(input("Whats you height?"))
+mass = float(input("Whats your weight?"))
+height = float(input("Whats you height(m)?"))
 if height > 2.3:
     print("Please enter in meters!")
-body_mass_index = weight / height * height
-if body_mass_index < 18.5:
-    print("You are underweight!")
-elif body_mass_index > 18.5:
-    print("You are in ideal weight!")
-elif body_mass_index > 24.9:
-    print("You are overweight!")
-
-# 1. Programm küsib sisendit: (input)
-# "What's your name?:" ja "Where do you study?:"
-# Nimi ja kool on nõutud(Ei tohi o.lla tühjad).
-# Kui nimi on puudu peab programm printima "Name was not inserted!",
-# kui kooli nimi on puudu tuleb printida "School was not inserted!"
-# Lõpus peaks programm printima "(Sisestatud nimi), welcome to (sisestatud kool)"
-# 2. Arvuta kehamassi indeks kasutades sisendit (input).
-# Üks sisend on mass(kg) teine sisend height(m). Lõpuks peab programm printima "(kehamassiindeks), (kehamassitüüp)" **kehamassitüüp printida väikeste tähtedega
+bmi = mass / (height * height)
+if bmi <= 18.49:
+    print(f"{bmi},You are underweight!")
+elif bmi >= 24.91:
+    print(f"{bmi}, You are overweight!")
+elif bmi > 18.5 or bmi < 24.9:
+    print(f"{bmi},You are in ideal weight!")
