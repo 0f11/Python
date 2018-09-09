4deec75408c68cf83134e41006112a177b335a3c
Rommi Parman <rommi.parman@ttu.ee>
2018-09-09 11:04:30 +0300

    v3

--- ex01_hello/hello.py
+++ ex01_hello/hello.py
@@ -18,9 +18,9 @@ else:
 mass = float(input("Whats your weight(kg)?"))
 height = float(input("Whats you height(m)?"))
 kehamassiindeks = mass / (height * height)
-if bmi <= 18.49:
+if kehamassiindeks <= 18.49:
     print(f"{kehamassiindeks},underweight!")
-elif bmi >= 24.91:
+elif kehamassiindeks >= 24.91:
     print(f"{kehamassiindeks}, overweight!")
-elif bmi > 18.5:
+elif kehamassiindeks > 18.5:
     print(f"{kehamassiindeks},ideal weight!")
