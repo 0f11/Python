09a0c7f30749dca728cbc39582defd6498db22c2
Rommi Parman <rommi.parman@ttu.ee>
2018-09-09 12:58:52 +0300

    v1

--- xp01_janguru/janguru.py
+++ xp01_janguru/janguru.py
@@ -8,7 +8,7 @@ Täiesti random
 x1, v1, x2, v2 = map(int, input().split())
 
 # Note: 0 <= x1 < x2 <= 10000
-# x1 + k.v1 = x2 + k.v20
+# x1 + k.v1 = x2 + k.v20,
 
 if (v2 < v1) and ((x2 - x1) % (v2 - v1)) == 0:
     print("YES")
