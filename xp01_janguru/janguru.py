aa86055e97e8286bee80e686bea0566fb67018b7
Rommi Parman <rommi.parman@ttu.ee>
2018-09-11 15:13:09 +0300

    v1

--- xp01_janguru/janguru.py
+++ xp01_janguru/janguru.py
@@ -2,16 +2,21 @@
 janguru.py.
 
 Täiesti random
-
 """
 pos1, jump_distance1, sleep1, pos2, jump_distance2, sleep2 = map(int, input().split())
 pos_x1 = pos1 + ((jump_distance1 / sleep1) * sleep1)
 pos_x2 = pos2 + ((jump_distance2 / sleep2) * sleep2)
 meeting = pos_x1 == pos_x1
+
+counter = 1
+
 while meeting == False:
-   # print (f"{pos_x2}")
-   int((pos_x1 + jump_distance1)) and int((pos_x2 + jump_distance2))
+    counter =+ 1
+    int((pos_x1 + jump_distance1)) and int((pos_x2 + jump_distance2))
+
+    print(f"{counter}")
 if meeting == True:
-    print(f"{pos_x2}")        
+    counter =+ 1
+    print(f"{counter}")
 else:
     print("-1")
