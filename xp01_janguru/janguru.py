cb1709bf2477f5c54a31a9dab9557c7de62aad30
Rommi Parman <rommi.parman@ttu.ee>
2018-09-12 12:18:01 +0300

    v1

--- xp01_janguru/janguru.py
+++ xp01_janguru/janguru.py
@@ -6,17 +6,18 @@ Täiesti random
 pos1, jump_distance1, sleep1, pos2, jump_distance2, sleep2 = map(int, input().split())
 pos_x1 = pos1 + ((jump_distance1 / sleep1) * sleep1)
 pos_x2 = pos2 + ((jump_distance2 / sleep2) * sleep2)
-meeting = pos_x1 == pos_x1
-
-counter = 1
-
-while meeting == False:
-    counter =+ 1
-    int((pos_x1 + jump_distance1)) and int((pos_x2 + jump_distance2))
-
-    print(f"{counter}")
-if meeting == True:
-    counter =+ 1
-    print(f"{counter}")
-else:
+if pos1 <= pos2 and jump_distance1 <= jump_distance2 and ((pos2 - pos1) % (jump_distance2 - jump_distance1) == 0):
     print("-1")
+while pos_x1 != pos_x2:
+    pos_x1 = pos_x1 + jump_distance1
+    pos_x2 = pos_x2 + jump_distance2
+
+print(f"{pos_x1}")
+"""
+meet_me(1, 2, 1, 2, 1, 1) => 3
+meet_me(1, 2, 3, 4, 5, 5) => -1
+meet_me(10, 7, 7, 5, 8, 6) => 45
+meet_me(100, 7, 4, 300, 8, 6) => 940
+meet_me(1, 7, 1, 15, 5, 1) => 50
+meet_me(0, 1, 1, 1, 1, 1) => -1
+"""
--- xp01_janguru/test2.py
+++ xp01_janguru/test2.py
@@ -1,2 +1,6 @@
-test = 5%2
-print(test)
+pos1, jump_distance1, sleep1, pos2, jump_distance2, sleep2 = map(int, input().split())
+pos_x1 = pos1 + ((jump_distance1 / sleep1) * sleep1)
+pos_x2 = pos2 + ((jump_distance2 / sleep2) * sleep2)
+pos_x1 = pos_x1 + jump_distance1
+pos_x2 = pos_x2 + jump_distance2
+print(f"{pos_x1}{pos_x2}")
