d1de48ebc6de6970f8dff4c7fe1d76c2047311b3
Rommi Parman <rommi.parman@ttu.ee>
2018-09-13 14:06:36 +0300

    v4

--- xp01_janguru/janguru.py
+++ xp01_janguru/janguru.py
@@ -4,33 +4,42 @@ janguru.py.
 Täiesti random
 """
 
-#pos1, jump_distance1, sleep1, pos2, jump_distance2, sleep2 = map(int, input().split())
+
+# pos1, jump_distance1, sleep1, pos2, jump_distance2, sleep2 = map(int, input().split())
 
 
 def meet_me(pos1, jump_distance1, sleep1, pos2, jump_distance2, sleep2):
-
-    map(int, input().split())
+    # Jänkude algpositsioonid
+    hype1 = (jump_distance1 / sleep1) * sleep1
+    hype2 = (jump_distance2 / sleep2) * sleep2
+    pos_x1 = pos1 + hype1
+    pos_x2 = pos2 + hype2
     if pos2 - pos1 and jump_distance2 - jump_distance1 == 0:
         print("-1")
         exit("Ei kohtu")
     if pos1 <= pos2 and jump_distance1 <= jump_distance2 and ((pos2 - pos1) % (jump_distance2 - jump_distance1) == 0):
         print("-1")
         exit("Ei kohtu")
-    hype1 = (jump_distance1 / sleep1) * sleep1
-    hype2 = (jump_distance2 / sleep2) * sleep2
-    pos_x1 = pos1 + hype1
-    pos_x2 = pos2 + hype2
+
     while pos_x1 != pos_x2:
         pos_x1 = pos_x1 + jump_distance1
         pos_x2 = pos_x2 + jump_distance2
-        return
     print(f"{pos_x1}")
+
+
+# pos1, jump_distance1, sleep1, pos2, jump_distance2, sleep2 = map(int, input().split())
+# meet_me(1, 2, 1, 2, 1, 1)
+# meet_me(1, 2, 3, 4, 5, 5)
+meet_me(10, 7, 7, 5, 8, 6)
+# meet_me(100, 7, 4, 300, 8, 6)
+# meet_me(1, 7, 1, 15, 5, 1)
+# meet_me(0, 1, 1, 1, 1, 1)
 
 
 # hype1 = (jump_distance1 / sleep1) * sleep1
 # hype2 = (jump_distance2 / sleep2) * sleep2
 # pos_x1 = pos1 + hype1
-# pos_x2 = pos2 + hype2
+# pos_x2 = pos2 + hype2.
 # if pos2 - pos1 and jump_distance2 - jump_distance1 == 0:
 #    print("-1")
 #    exit("Ei kohtu")
