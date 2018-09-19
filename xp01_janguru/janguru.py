b7d0894a32ab6fd6724b6fea1132bca298323fe0
Rommi Parman <rommi.parman@ttu.ee>
2018-09-19 13:44:29 +0300

    v8

--- xp01_janguru/janguru.py
+++ xp01_janguru/janguru.py
@@ -6,7 +6,6 @@ Olen kulutanu sellele juba 3 nädalat, vaadates sellele ajale tagasi, ei khetse 
 
 
 def meet_me(pos1, jump_distance1, sleep1, pos2, jump_distance2, sleep2):
-    """ Jänkude algpositsioonid. """
     hype1 = (jump_distance1 / sleep1) * sleep1
     hype2 = (jump_distance2 / sleep2) * sleep2
     pos_x1 = pos1 + hype1
@@ -30,12 +29,12 @@ def meet_me(pos1, jump_distance1, sleep1, pos2, jump_distance2, sleep2):
 
 
 meet_me(1, 2, 1, 2, 1, 1)  # => 3
-#meet_me(1, 2, 3, 4, 5, 5)  # => -1
+meet_me(1, 2, 3, 4, 5, 5)  # => -1
 meet_me(10, 7, 7, 5, 8, 6)  # => 45
 meet_me(300, 8, 6, 100, 7, 4)  # => 940
 meet_me(1, 7, 1, 15, 5, 1)  # => 50
 meet_me(0, 1, 1, 1, 1, 1)  # => -1
-#meet_me(918674, 5527, 5704, 1919, 2763, 2833)  # => 141459230
+# meet_me(918674, 5527, 5704, 1919, 2763, 2833)  # => 141459230
 meet_me(911, 19, 25, 2, 26, 32)  # => 13964
 meet_me(941, 27, 47, 26, 17, 28)  # => 17060
 meet_me(934, 22, 62, 82, 39, 34)  # => 1330
