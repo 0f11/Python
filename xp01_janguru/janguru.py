f11ed5c7acf96755f1dcedbfcb35904b8238ed47
Rommi Parman <rommi.parman@ttu.ee>
2018-09-19 14:59:17 +0300

    v13

--- xp01_janguru/janguru.py
+++ xp01_janguru/janguru.py
@@ -12,16 +12,14 @@ def meet_me(pos1, jump_distance1, sleep1, pos2, jump_distance2, sleep2):
     aeg2 = sleep2
     if pos2 - pos1 and jump_distance2 - jump_distance1 == 0:
         return print("-1")
-    if jump_distance1 <= jump_distance2 and pos1 <= pos2 and int((jump_distance1 / sleep1) < int(jump_distance2 / sleep2)):
+    if jump_distance1 <= jump_distance2 and pos1 <= pos2 and int(
+            (jump_distance1 / sleep1) < int(jump_distance2 / sleep2)):
         return print("-1")
     while pos_x1 != pos_x2:
         aeg = aeg + 1
         aeg2 = aeg2 + 1
         if aeg % sleep1 == 0:
             pos_x1 = pos_x1 + jump_distance1
-            # print(f"{pos_x1}")
         if aeg2 % sleep2 == 0:
             pos_x2 = pos_x2 + jump_distance2
-        if pos_x1 == pos_x2:
-            break
     print(f"{pos_x2}")
