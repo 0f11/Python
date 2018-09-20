d6418b515390287f4fb9bf8e0d2721600b666e18
Rommi Parman <rommi.parman@ttu.ee>
2018-09-20 14:50:40 +0300

    v15

--- xp01_janguru/janguru.py
+++ xp01_janguru/janguru.py
@@ -1,19 +1,26 @@
+"""
+janguru.py.
+
+Olen kulutanu sellele juba 3 nädalat, vaadates sellele ajale tagasi, ei kahetse ma miskit.
+"""
+
+
 def meet_me(pos1, jump_distance1, sleep1, pos2, jump_distance2, sleep2):
     pos_x1 = pos1 + jump_distance1
     pos_x2 = pos2 + jump_distance2
-    aeg = sleep1
-    aeg2 = sleep2
     x = 0
-    meet1 = pos_x2 + jump_distance2 * x
-    meet2 = pos_x1 + jump_distance1 * x
-    if pos2 - pos1 and jump_distance2 - jump_distance1 == 0:
+    if pos2 - pos1 == 0 and jump_distance2 - jump_distance1 == 0:
         return print("-1")
-    if jump_distance1 <= jump_distance2 and pos1 <= pos2 and int(
-            (jump_distance1 / sleep1) < int(jump_distance2 / sleep2)):
+    if pos1 <= pos2 and jump_distance1 <= jump_distance2 and (jump_distance1 / sleep1) < (jump_distance2 / sleep2):
         return print("-1")
-    while meet1 != meet2:
-        aeg = aeg + 1
-        aeg2 = aeg2 + 1
+    if jump_distance1 / sleep1 == jump_distance2 / sleep2:
+        return print("-1")
+    while pos_x1 != pos_x2:
         x = x + 1
-        meet1 = pos_x1 + jump_distance1 * x
-        meet2 = pos_x2 + jump_distance2 * x
+        if x % sleep1 == 0:
+            pos_x1 = pos_x1 + jump_distance1
+        if x % sleep2 == 0:
+            pos_x2 = pos_x2 + jump_distance2
+        if pos_x1 == pos_x2:
+            break
+    print(f"{pos_x2}")
