243fca88b2f5a3ac577a558712d217af492d8d18
Rommi Parman <rommi.parman@ttu.ee>
2018-09-25 15:52:54 +0300

    v6

--- ex03_id/id_code.py
+++ ex03_id/id_code.py
@@ -196,8 +196,6 @@ def get_data_from_id(id_code: str):
     :param id_code: str
     :return: str
     """
-    if check_your_id(id_code) == False:
-        return "Given invalid ID code!"
     year = id_code[1:3]
     gender_number = id_code[0:1]
     if int(gender_number) <= 2:
