f21d272216b54b663150b76a4692b691c1db2a51
Rommi Parman <rommi.parman@ttu.ee>
2018-09-27 09:14:13 +0300

    v1

---
+++ pr03_nomination/nomination.py
@@ -0,0 +1,47 @@
+import time
+
+"""
+Nomination.py.
+
+"""
+
+
+def main():
+    name = ask_name()
+    progress_bar("Setting up the nominee", 5)
+    print_ok()
+
+
+def ask_name():
+    while True:
+        name = input("What is your full name, dear fellow?").strip()
+        is_title = name.istitle()
+        is_spaced = name.find(" ") != -1
+        is_alpha = name.replace(" ", "").replace("-", "").isalpha()
+        if is_title and is_spaced and is_alpha:
+            return name
+        else:
+            print("Sorry, try again.")
+
+
+def progress_bar(process_name, seconds):
+    cycle_time = seconds / 20
+
+    chr_limit = 25
+    is_long = len(process_name) > chr_limit - 2
+
+    if is_long:
+        process_name = f"{process_name[:chr_limit - 5]}..."
+
+    for i in range(21):
+        print(f"\r[{'|' * i:-<20}] | Process: {process_name!r:.25} {0.05 * i:4.0%}", end='')
+        time.sleep(cycle_time)
+    print()
+
+
+def print_ok():
+    print("Nominee listed.")
+
+
+if __name__ == "__main__":
+    main()
