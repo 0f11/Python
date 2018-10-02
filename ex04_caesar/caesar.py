0c33dca9cafd51a45884610ccfa34e5ef6833487
Rommi Parman <rommi.parman@ttu.ee>
2018-10-02 13:46:01 +0300

    v2

--- ex04_caesar/caesar.py
+++ ex04_caesar/caesar.py
@@ -46,7 +46,16 @@ if __name__ == "__main__":
     # simple tests
     print(encode("hello", 1))  # ifmmp
     print(decode("ifmmp", 1))  # hello
-
+    print(decode("ifmmp", 50))
+    print(encode("hello", 50))
+    print(decode("ifmmp", -3))
+    print(encode("hello", -4))
+    print(decode("ifmmp", 30))
+    print(encode("hello", 27))
+    print(decode("iFMmP", 1))
+    print(encode("HEllO", 1))
+    print(decode("iFM3::134;", 3))
+    print(encode("H::13EllO", 1))
     # WRITE THE REMAINING EXAMPLES YOURSELF!
 
     # larger shift
