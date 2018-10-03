4e31110979713301d8401a1e4541be0fa0e19687
Rommi Parman <rommi.parman@ttu.ee>
2018-10-03 17:30:31 +0300

    v5

--- ex04_caesar/caesar.py
+++ ex04_caesar/caesar.py
@@ -39,6 +39,8 @@ def encode(message: str, shift: int, alphabet="abcdefghijklmnopqrstuvwxyz") -> s
             kood += x1
     return kood
 
+    pass
+
 
 def decode(message: str, shift: int, alphabet="abcdefghijklmnopqrstuvwxyz") -> str:
     """
@@ -82,32 +84,3 @@ def decode(message: str, shift: int, alphabet="abcdefghijklmnopqrstuvwxyz") -> s
     return kood2
 
     pass
-
-
-if __name__ == "__main__":
-    # simple tests
-    print(encode("hello", 1))  # ifmmp
-    print(decode("ifmmp", 1))  # hello
-    print(decode("ifmmp", 50))
-    print(encode("hello", 50))
-    print(decode("ifmmp", -3))
-    print(encode("hello", -4))
-    print(decode("ifmmp", 30))
-    print(encode("hello", 27))
-    print(decode("iFMmP", 1))
-    print(encode("HEllO", 1))
-    print(decode("iFM3::134;", 3))
-    print(encode("H::13EllO", 1))
-    # WRITE THE REMAINING EXAMPLES YOURSELF!
-
-    # larger shift
-
-    # negative shift
-
-    # shift > alphabet.length
-
-    # case sensitivity
-
-    # misc symbols (.,:; etc.)
-
-    # ...
