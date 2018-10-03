17d1acefee48746c50ef52ee087f600c7dde4a98
Rommi Parman <rommi.parman@ttu.ee>
2018-10-03 19:14:50 +0300

    v5

--- ex04_caesar/caesar.py
+++ ex04_caesar/caesar.py
@@ -1,5 +1,6 @@
 """Caesar cipher."""
 
+
 def encode(message: str, shift: int, alphabet="abcdefghijklmnopqrstuvwxyz") -> str:
     """
     Encode the given message using the Caesar cipher principle.
