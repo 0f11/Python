dc4d8f83a9b635bd3c53c61e42b720dbc1a15e92
Rommi Parman <rommi.parman@ttu.ee>
2018-10-04 19:18:12 +0300

    v6

--- pr04_adding/adding.py
+++ pr04_adding/adding.py
@@ -57,14 +57,17 @@ def add_list_elements(int_list):
     """
     sort_list([int_list])
     new_int_list = []
-    while len([int_list]) <= 2 and len([int_list]) % 2 >= 1 and get_max_element(int_list) is not None and \
-            get_min_element(int_list) is not None:
-
-        uus1 = get_max_element(int_list)
-        uus2 = get_min_element(int_list)
-        uus3 = int(uus1) + int(uus2)
-        new_int_list.append(uus3)
-        # print(int_list)
+    while get_max_element(int_list) is not None and get_min_element(int_list) is not None:
+        if len(int_list) % 2 == 0:
+            uus1 = get_max_element(int_list)
+            uus2 = get_min_element(int_list)
+            uus3 = int(uus1) + int(uus2)
+            new_int_list.append(uus3)
+        if len(int_list) % 3 == 0:
+            uus1 = get_max_element(int_list)
+            uus2 = get_min_element(int_list)
+            uus3 = int(uus1) + int(uus2)
+            new_int_list.append(uus3)
         if len(int_list) >= 1:
             del int_list[0]
         if len(int_list) >= 1:
