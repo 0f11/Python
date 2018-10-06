19161d4a01cf04fa857a38ca0536385a2a1f45b3
Rommi Parman <rommi.parman@ttu.ee>
2018-10-06 20:08:58 +0300

    v116

--- xp01_janguru/janguru.py
+++ xp01_janguru/janguru.py
@@ -22,7 +22,7 @@ def meet_me(pos1, jump_distance1, sleep1, pos2, jump_distance2, sleep2):
     p2 = pos2
     k1 = jump_distance1 / sleep1
     k2 = jump_distance2 / sleep2
-    if (jump_distance1 == jump_distance2 and sleep1 == sleep2) or (pos1 <= pos2 and k1 <= k2) or (
+    if (jump_distance1 <= jump_distance2 and sleep1 == sleep2) or (pos1 <= pos2 and k1 <= k2) or (
             pos1 >= pos2 and k1 >= k2):
         return -1
     while p1 != p2:
@@ -36,7 +36,7 @@ def meet_me(pos1, jump_distance1, sleep1, pos2, jump_distance2, sleep2):
 
 
 if __name__ == "__main__":
-    print(meet_me(1, 1, 1, 1, 1, 1))
+    print(meet_me(1, 2, 1, 2, 1, 1))
     print(meet_me(1, 2, 3, 4, 5, 5))
     print(meet_me(10, 7, 7, 5, 8, 6))
     print(meet_me(100, 7, 4, 300, 8, 6))
