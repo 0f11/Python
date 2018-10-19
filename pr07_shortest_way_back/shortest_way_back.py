c4cbe8dff19228b1edaccf7942a7d85dfb9dda31
Rommi Parman <rommi.parman@ttu.ee>
2018-10-19 15:28:54 +0300

    v1

--- pr07_shortest_way_back/shortest_way_back.py
+++ pr07_shortest_way_back/shortest_way_back.py
@@ -36,39 +36,25 @@ def shortest_way_back(path: str) -> str:
 
         elif direction == "W":
             long = long + d["W"][1]
-
     tagasitee = []
-    if lat > 0:
-        tagasitee.append("S"*abs(lat))
-    elif lat < 0:
-        tagasitee.append("N"*abs(lat))
-    elif long > 0:
-        tagasitee.append("W"*abs(long))
-    elif long < 0:
-        tagasitee.append("E"*abs(long))
-    return "".join(tagasitee)
-    # while True:
-    #     if lat > 0:
-    #         lat = lat + d["S"][0]
-    #         tagasitee.append("S")
-    #
-    #     elif lat < 0:
-    #         lat = lat + d["N"][0]
-    #         tagasitee.append("N")
-    #
-    #     if long > 0:
-    #         long = long + d["W"][1]
-    #         tagasitee.append("W")
-    #
-    #     elif long < 0:
-    #         long = long + d["E"][1]
-    #         tagasitee.append("E")
-    #
-    #     if lat == 0 and long == 0:
-    #         break
 
-    # return "".join(tagasitee)
+    while True:
+        if lat > 0:
+            tagasitee.append(lat * "S")
+            lat = lat + (lat * d["S"][0])
+        elif lat < 0:
+            tagasitee.append(abs(lat) * "N")
+            lat = lat + (abs(lat) * d["N"][0])
+        if long > 0:
+            tagasitee.append(abs(long) * "W")
+            long = long + (abs(long) * d["W"][1])
+        elif long < 0:
+            tagasitee.append(abs(long) * "E")
+            long = long + (abs(long) * d["E"][1])
+        if lat == 0 and long == 0:
+            break
 
+    print("".join(tagasitee))
 
 if __name__ == '__main__':
     shortest_way_back("NNN")  # == "SSS"
@@ -76,5 +62,5 @@ if __name__ == '__main__':
     shortest_way_back("E")  # == "W"
     shortest_way_back("WWWW")  # == "EEEE"
     shortest_way_back("")  # == ""
-    shortest_way_back("NESW")# == ""
+    shortest_way_back("NESW")  # == ""
     shortest_way_back("NNEESEW") in ["SWW", "WSW", "WWS"]
