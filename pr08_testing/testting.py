ebaab598f5c1896f5f2854cd17d538bdc02fe997
rommi.parman <rommi.parman@ttu.ee>
2018-10-29 17:44:27 +0200

    v1

--- pr08_testing/testting.py
+++ pr08_testing/testting.py
@@ -74,17 +74,32 @@ def test_north():
     assert shortest_way_back("S") == "N"
 
 
+def test_north_long():
+    assert shortest_way_back("SSS") == "NNN"
+
+
 def test_south():
     assert shortest_way_back("N") == "S"
+
+
+def test_south_long():
+    assert shortest_way_back("NNN") == "SSS"
 
 
 def test_west():
     assert shortest_way_back("E") == "W"
 
 
+def test_west_long():
+    assert shortest_way_back("EEE") == "WWW"
+
+
 def test_east():
     assert shortest_way_back("W") == "E"
 
+
+def test_east():
+    assert shortest_way_back("WWW") == "EEE"
 
 def test_empty_string():
     assert shortest_way_back("") == ""
