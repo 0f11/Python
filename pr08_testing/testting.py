690caa2451307f6257ea192f658005412206e022
rommi.parman <rommi.parman@ttu.ee>
2018-10-29 17:25:38 +0200

    v1

--- pr08_testing/testting.py
+++ pr08_testing/testting.py
@@ -1,23 +1,31 @@
 """Testing.py."""
 
-import pytest
-import shortest_way_back
+from shortest_way_back import shortest_way_back
 import random
 
+def test_north():
+    assert shortest_way_back("N") == "S"
 
-def test_shortest_way_back():
-    """
-    Test.
 
-    :return:
-    """
-    assert "SSS" in shortest_way_back("NNN")
-    assert "NN" in shortest_way_back("SS")
-    assert "W" in shortest_way_back("E")
-    assert "EEEE" in shortest_way_back("WWWW")
-    assert "" in shortest_way_back("")
-    assert "" in shortest_way_back("NESW")
-    assert shortest_way_back("NNEESEW") in ["SWW", "WSW", "WWS"]
+def test_south():
+    assert shortest_way_back("S") == "N"
 
-def test_random():
-    assert random in shortest_way_back(random)
+
+def test_west():
+    assert shortest_way_back("W") == "E"
+
+
+def test_east():
+    assert shortest_way_back("EE") == "WW"
+
+
+def test_empty_string():
+    assert shortest_way_back("") == ""
+
+
+def test_finish_home():
+    assert shortest_way_back("WENS") == ""
+
+
+
+
