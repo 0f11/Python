e6e34bd718f512e23235f8b45c68d3493599eeff
rommi.parman <rommi.parman@ttu.ee>
2018-10-30 10:59:10 +0200

    v56

--- pr08_testing/testing.py
+++ pr08_testing/testing.py
@@ -71,46 +71,101 @@ def midaiganes(lat, long):
 
 
 def test_north():
+    """
+    Test north.
+
+    :return:
+    """
     assert shortest_way_back("S") == "N"
 
 
 def test_north_long():
+    """
+    Test north long.
+
+    :return:
+    """
     assert shortest_way_back("SSS") == "NNN"
 
 
 def test_south():
+    """
+    Test south.
+
+    :return:
+    """
     assert shortest_way_back("N") == "S"
 
 
 def test_south_long():
+    """
+    Test south long.
+
+    :return:
+    """
     assert shortest_way_back("NNN") == "SSS"
 
 
 def test_west():
+    """
+    Test west.
+
+    :return:
+    """
     assert shortest_way_back("E") == "W"
 
 
 def test_west_long():
+    """
+    test west long.
+
+    :return:
+    """
     assert shortest_way_back("EEE") == "WWW"
 
 
 def test_east():
+    """
+    Test east.
+
+    :return:
+    """
     assert shortest_way_back("W") == "E"
 
 
 def test_east_long():
+    """
+    Test east long.
+
+    :return:
+    """
     assert shortest_way_back("WWW") == "EEE"
 
 
 def test_empty_string():
+    """
+    Test empty.
+
+    :return:
+    """
     assert shortest_way_back("") == ""
 
 
 def test_finish_home():
+    """
+    Test finish home.
+
+    :return:
+    """
     assert shortest_way_back("WENS") == ""
 
 
 def test_only_valid():
+    """
+    Test random.
+
+    :return:
+    """
     for i in range(100):
         path = random.choices("NSWE", k=50)
         result = sorted(shortest_way_back(path))
